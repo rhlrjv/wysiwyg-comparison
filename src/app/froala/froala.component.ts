7ea36d15a3c9dc06afb4d1d4d9b142a1d6c1ef8d
Ha Nguyen <hanguyen@dbs.com>
2018-03-13 14:24:47 +0800

    Update froala options to make it work with markdown
    Signed-off-by: Rahul Rajeev <rrajeev@pivotal.io>

--- src/app/froala/froala.component.ts
+++ src/app/froala/froala.component.ts
@@ -16,35 +16,47 @@ export class FroalaComponent {
   public froalaOptions: Object = {
     charCounterCount: true,
     charCounterMax: 4000,
-    colorsText: ['#000000', '#ff0000', '#00ff00', 'REMOVE'],
-    colorsBackground: ['#FFFF00', 'REMOVE'],
     colorsHEXInput: false,
     emoticonsSet: [],
     fileUpload: false,
     fontFamilySelection: false,
     fontSize: ['8', '10', '12', '14', '18', '30', '60', '96'],
     fontSizeSelection: true,
-    toolbarButtons: ['bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|',
-      'color', '|',
-      'paragraphFormat', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '|',
-      'insertLink', 'insertImage', 'insertTable', '|',
-      'insertHR', 'clearFormatting', 'undo', 'redo'],
-    pastePlain: true,
+    toolbarButtons: ['bold', 'italic', '|',
+      'paragraphFormat', 'formatOL', 'formatUL', 'quote', '|',
+      'insertLink', 'insertImage', '|',
+      'insertHR', 'undo', 'redo'],
+    paragraphFormat: {
+      N: 'Normal',
+      H1: 'Heading 1',
+      H2: 'Heading 2',
+      H3: 'Heading 3',
+    },
+    LinkEditButtons: ['linkOpen', 'linkEdit', 'linkRemove'],
+    LinkInsertButtons: ['linkBack'],
+    linkList: [],
+    pastePlain: false,
     pasteDeniedAttrs: ['class', 'id', 'style', 'font-family', 'font-size', 'color'],
     pasteAllowedStyleProps: [],
     htmlRemoveTags: ['script', 'style', 'base'],
     htmlExecuteScripts: false,
-    htmlAllowedTags: ['a', 'br', 'dd', 'div', 'dl', 'dt', 'em', 'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'hr', 'i', 'img', 'li', 'ol', 'p', 'span', 'strike', 'strong', 'tbody', 'td', 'tfoot', 'th', 'thead', 'title', 'tr', 'track', 'u', 'ul'],
-    wordPasteModal: false
+    htmlAllowedTags: ['a', 'br', 'div', 'dl', 'em', 'h1', 'h2', 'h3', 'hr', 'i', 'img', 'li', 'ol', 'p', 'span', 'strong', 'ul', 'blockquote'],
+    shortcutsEnabled: ['bold', 'italic', 'indent', 'outdent', 'undo', 'redo', 'insertImage', 'createLink'],
+    wordPasteModal: false,
+    imageDefaultAlign: 'center',
+    imageDefaultDisplay: 'block',
+    imageEditButtons: ['imageReplace', 'imageRemove'],
+    imageResize: false,
+    imageTextNear: false,
+    imageDefaultWidth: 0
   }
 
   constructor() {
-    this.turndownService = new TurndownService();
-
-    this.converter = new showdown.Converter({
-      'tables': true,
-      'strikethrough': true
+    this.turndownService = new TurndownService({
+      headingStyle: 'atx'
     });
+
+    this.converter = new showdown.Converter();
     this.converter.setFlavor('github');
   }
 
